6d503496cec7873e858985f401a47f4d0132367e
Filip Paryż <paryz.filip@gmail.com>
2024-12-21 19:19:55 +0100

    Added simple install script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
+echo "Adding nvim repository"
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update -y
+sudo apt upgrade -y
 
-sudo apt update
-
-sudo apt upgrade
-
+echo "Installing packages"
 # Install packages
-sudo apt install git tmux zsh ripgrep xclip unzip make gcc nvim fd-find -y
+sudo apt install -y git tmux zsh ripgrep xclip unzip make gcc fd-find nvim
 
 # Install NVIM
 #curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -15,6 +15,17 @@ sudo apt install git tmux zsh ripgrep xclip unzip make gcc nvim fd-find -y
 
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
+curl -Lo ~/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf ~/lazygit.tar.gz ~/lazygit
+sudo install ~/lazygit -D -t /usr/local/bin/
+rm -rf ~/lazygit ~/lazygit.tar.gz
+
+echo "Stowing your config..."
+cd ~/.dotfiles/
+stow lazygit
+stow nvim
+stow zsh
+stow tmux
+stow oh-my-zsh
+
+echo "All done 👍"
