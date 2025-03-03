c5ccf2305caa87e738b84e7b1c0fea322156d0ee
Filip Paryż <paryz.filip@gmail.com>
2025-03-03 17:08:40 +0100

    Transitioned all my config to modular kickstart base

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -20,29 +20,28 @@ require('lazy').setup({
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
   --
 
-
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
+  require 'lua/kickstart/plugins/gitsigns',
 
-  require 'kickstart/plugins/which-key',
+  require 'lua/kickstart/plugins/which-key',
 
-  require 'kickstart/plugins/telescope',
+  require 'lua/kickstart/plugins/telescope',
 
-  require 'kickstart/plugins/lspconfig',
+  require 'lua/kickstart/plugins/lspconfig',
 
-  require 'kickstart/plugins/conform',
+  require 'lua/kickstart/plugins/conform',
 
-  require 'kickstart/plugins/cmp',
+  require 'lua/kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
+  -- require 'lua/kickstart/plugins/tokyonight',
 
-  require 'kickstart/plugins/todo-comments',
+  require 'lua/kickstart/plugins/todo-comments',
 
-  require 'kickstart/plugins/mini',
+  require 'lua/kickstart/plugins/mini',
 
-  require 'kickstart/plugins/treesitter',
+  require 'lua/kickstart/plugins/treesitter',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -64,6 +63,10 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- { import = 'custom.plugins' },
+
+  require 'lua/custom/snacks',
+
+  require 'lua/custom/theme',
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
