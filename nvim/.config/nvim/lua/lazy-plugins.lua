37850768c0a3cb0cf8c2109eedc66b061a4f0254
Filip Paryz <paryz.filip@gmail.com>
2025-05-19 23:23:19 +0200

    New nvim plugins - sort and hardtime

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -64,9 +64,13 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- { import = 'custom.plugins' },
 
+  require 'custom/sort',
+
   require 'custom/snacks',
 
   require 'custom/theme',
+
+  require 'custom/hardtime',
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
